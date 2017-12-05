21b4d8b48c1981660d9c6c6d336c7ac14795f6c5
Jake <Jake@wirelessprv-10-195-229-42.near.illinois.edu>
2017-12-05 16:10:47 -0600

    Coloring attempt

--- MP7/src/ACSIIPrinter.java
+++ MP7/src/ACSIIPrinter.java
@@ -3,51 +3,50 @@ import java.io.IOException;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
+
 public class ACSIIPrinter {
 
 	static BufferedImage image = null;
 	static File f = null;
 	static int width = 230;
 	static int height = 219;
-	static int total = 0;
-	static int average = 0;
-	public static int[][] charImage = new int[1000][1000];
+	static int res = 5;	
+
 
 	
 	public static void main(String args[]) throws IOException {
 
 		
 		try {
-			f = new File("resources/appleicon.png");
-			image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+			f = new File("resources/caticon.png");
+			image = new BufferedImage(width / res, height / res, BufferedImage.TYPE_INT_RGB);
 			image = ImageIO.read(f);
 		} catch (IOException e) {
 			System.out.println("Error: " + e);
 	  }
-		for (int i = 0; i < image.getHeight(); i++) {
+		for (int i = 0; i < image.getHeight(); i += res) {
 			System.out.println("");
-			for (int j = 0; j < image.getWidth(); j++) {
-				if (image.getRGB(j, i) < -1) {
-				System.out.print("0 ");
+			for (int j = 0; j < image.getWidth(); j += res) {
+				
+				/* int pixel = image.getRGB(j, i);
+				int fifthMax = 0x00FFFFFF / 5;
+					if (pixel < fifthMax) {System.out.print("  ");} 
+					else if ((pixel > fifthMax) && (pixel < 2 * fifthMax)) { System.out.print(". ");}
+					else if ((pixel > 2 * fifthMax) && (pixel < 3 * fifthMax)) {System.out.print(": ");}
+					else if ((pixel > 3 * fifthMax) && (pixel < 4 * fifthMax)) {System.out.print("! ");}
+					else {System.out.print("# ");} */
+				
+				//System.out.print(image.getRGB(j, i));
+				if (Math.abs(image.getRGB(j, i)) < 0x00FFFFFF / 5) {
+				System.out.print("  ");
+				} else if ((Math.abs(image.getRGB(j, i)) < 2 * 0x00FFFFFF / 5) &&
+				(Math.abs(image.getRGB(j, i)) > 0x00FFFFFF / 5)) {
+					System.out.print("' ");
 				} else {
-					System.out.print("1 ");
+					System.out.print("# ");
 				}
 			}
 		}
-	}
-		public static int[][] pixelArray (BufferedImage image) {
-			for (int i = 0; i < image.getHeight(); i += image.getHeight() / 5) {
-				for (int j = 0; j < image.getWidth(); j += image.getWidth() / 5) {
-				
-						total = total + image.getRGB(i, j);
-						average = total / 25;
-			for (int m = 0; m < charImage.length; m++) {
-				for (int n = 0; n < charImage[0].length; n++) {
-					charImage[m][n] = average;
-					}
-				}
-			}
-		} return charImage;
 	}
 }
 
