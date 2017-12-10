4fbd50da334234cbd0ad1f9709f23f1e7f968f5b
Jake <Jake@wirelessprv-10-195-229-42.near.illinois.edu>
2017-12-10 15:54:10 -0600

    Added 6 "colors" Added more images Need to make a input system for inputing images and resolutions

--- MP7/src/ACSIIPrinter.java
+++ MP7/src/ACSIIPrinter.java
@@ -8,43 +8,37 @@ public class ACSIIPrinter {
 
 	static BufferedImage image = null;
 	static File f = null;
-	static int width = 230;
-	static int height = 219;
-	static int res = 5;	
+	static int width = 300;
+	static int height = 300;
+	static int res = 2;	
 
 
 	
 	public static void main(String args[]) throws IOException {
-
 		
 		try {
-			f = new File("resources/caticon.png");
-			image = new BufferedImage(width / res, height / res, BufferedImage.TYPE_INT_RGB);
+			f = new File("resources/will-smith-2.jpg");
+			image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 			image = ImageIO.read(f);
 		} catch (IOException e) {
 			System.out.println("Error: " + e);
 	  }
+	
+		
 		for (int i = 0; i < image.getHeight(); i += res) {
 			System.out.println("");
 			for (int j = 0; j < image.getWidth(); j += res) {
 				
-				/* int pixel = image.getRGB(j, i);
-				int fifthMax = 0x00FFFFFF / 5;
-					if (pixel < fifthMax) {System.out.print("  ");} 
-					else if ((pixel > fifthMax) && (pixel < 2 * fifthMax)) { System.out.print(". ");}
-					else if ((pixel > 2 * fifthMax) && (pixel < 3 * fifthMax)) {System.out.print(": ");}
-					else if ((pixel > 3 * fifthMax) && (pixel < 4 * fifthMax)) {System.out.print("! ");}
-					else {System.out.print("# ");} */
+				int pixel = (image.getRGB(j, i)) & 0xFFFFFF;
+				int sixthMax = 0x00FFFFFF / 6;
+					if (pixel < sixthMax) {System.out.print("% ");} 
+					else if ((pixel > sixthMax) && (pixel < 2 * sixthMax)) { System.out.print("# ");}
+					else if ((pixel > 2 * sixthMax) && (pixel < 3 * sixthMax)) {System.out.print("* ");}
+					else if ((pixel > 3 * sixthMax) && (pixel < 4 * sixthMax)) {System.out.print(": ");}
+					else if ((pixel > 4 * sixthMax) && (pixel < 5 * sixthMax)) {System.out.print(". ");}
+					else {System.out.print("  ");} 
 				
-				//System.out.print(image.getRGB(j, i));
-				if (Math.abs(image.getRGB(j, i)) < 0x00FFFFFF / 5) {
-				System.out.print("  ");
-				} else if ((Math.abs(image.getRGB(j, i)) < 2 * 0x00FFFFFF / 5) &&
-				(Math.abs(image.getRGB(j, i)) > 0x00FFFFFF / 5)) {
-					System.out.print("' ");
-				} else {
-					System.out.print("# ");
-				}
+				//System.out.print(pixel + " ");
 			}
 		}
 	}
